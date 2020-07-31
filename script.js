e7696933c27b5a153e8eab16a192e3774d712ea1
Rahul Bakshi <rahulbakshi3@gmail.com>
2020-08-01 00:44:41 +0530

    add new quote and tweet button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 //Get quote from Api
 async function getQuote(){
@@ -7,11 +11,35 @@ async function getQuote(){
     try{
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown';
+        } else{
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        if(data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove('long-quote');
+        }
+        
+        quoteText.innerText = data.quoteText;
+      
     }catch(error){
         getQuote();
-        console.log('damn its a error',error);
+       
 }
 }
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank'); 
+}
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
 //On load
 getQuote();
